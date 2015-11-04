6c59b42050f545e35ec287980b46bc54e04a59d1
thinq4yourself <joel@thinq4yourself.com>
2015-11-04 01:02:21 +0000

    updated grunt bump to include beep, boop, bop

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,16 @@
+module.exports = function(grunt) {
+
+// Project configuration.
 grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+		
   bump: {
     options: {
-      files: ['package.json', 'style.css'],
+      files: ['package.json', 'giftstarter/style.css'],
       updateConfigs: [],
       commit: true,
       commitMessage: 'Release v%VERSION%',
-      commitFiles: ['package.json', 'style.css'],
+      commitFiles: ['package.json', 'giftstarter/style.css'],
       createTag: true,
       tagName: 'v%VERSION%',
       tagMessage: 'Version %VERSION%',
@@ -18,3 +23,24 @@ grunt.initConfig({
     }
   }
 })
+
+// Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-angular-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-bump');
+
+
+// Built in versioning, Archer style (https://www.youtube.com/watch?v=C6NRA69SdoM)
+	grunt.registerTask('beep', ['bump:patch']);
+	grunt.registerTask('boop', ['bump:minor']);
+	grunt.registerTask('bop', ['bump:major']); // you better be sure you know what you're doing here. 00 bops given
+	grunt.registerTask('prebeep', ['bump:prepatch']);
+	grunt.registerTask('preboop', ['bump:preminor']);
+	grunt.registerTask('prebop', ['bump:premajor']);
+	grunt.registerTask('prerelease', ['bump:prerelease']);
+	// for a full list of bump commands, see https://www.npmjs.com/package/grunt-bump
+	
+
+};
